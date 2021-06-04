4318d075eb74da0e367e3d6ee4fa04408e86d892
abielk <abielk@sfu.ca>
2021-06-04 16:23:27 -0700

    php script that simulates compatible buildpack

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+echo nl2br("THIS IS AN AUTOMATED PHP SCRIPT WITH THE SOLE PURPOSE OF SIMULATING A COMPATIBLE HEROKU BUILDPACK\n");
+echo nl2br("REDIRECTING YOU NOW...\n");
+header("Location: calculator.html");
+exit();
+?>
